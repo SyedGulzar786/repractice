458716a408e5fa68311d7dabb2c002643d177f62
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-02-19 02:46:23 -0800

    js test

---
+++ app.js
@@ -0,0 +1,41 @@
+// var arr = [];
+// var arr = ["gulzar", "ali"];
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// var arr = [{
+//     name: "gulzar",
+//     age: 22
+// },{
+//     name: "ali",
+//     age: 23
+// }];
+
+// var arr = ["ssc", "hssc", "bsc", "msc"];
+// document.write(arr[0] + "<br>" + arr[1] + "<br>" + arr[2] + "<br>" + arr[3] + "<br>");
+
+// var arr = ["gulzar", "ali", "umer", "saad", "shahzad"];
+// var score = [320, 230, 480, 120, 420];
+// var total = 500;
+// var percentage = 0;
+// for (var i = 0; i < arr.length; i++) {
+//     percentage = (score[i] / total) * 100;
+//     document.write("Score of " + arr[i] + " is " + score[i] + ". Percentage: " + percentage + "%<br>");
+// }
+
+// var arr = ["green", "yellow", "black"];
+// var prom = prompt("Enter color you want to add in the beginning of array");
+// arr.pop(prom)
+// document.write(arr);
+
+// var arr = [120,394,563,78,9 ,15,10]
+// arr.sort(function(a, b){return a-b})
+
+// var arr = ["karachi","lahore","islamabad","pishawer","quetta"];
+// var city = arr.slice(2,4);
+// document.write(city);
+
+var arr = ["this", "is", "my", "cat"];
+var final = arr.join(" ");
+document.write(final);
+
+  
+  
